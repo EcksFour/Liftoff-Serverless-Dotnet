e550abe8ea419dff53c3223dbad38f3d2ca4cb8b
EcksFour <ecksfourr@gmail.com>
2024-08-20 18:47:36 -0400

    adding healthcheck controller endpoint

---
+++ src/Liftoff-Serverless/Controllers/HealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Liftoff_Serverless.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "test1", "test2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "id";
+        }
+    }
+}
